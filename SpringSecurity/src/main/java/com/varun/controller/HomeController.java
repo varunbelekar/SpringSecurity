92323f0aa5c5968421f904a7037a4b364b336e60
varun belekar <vbele@nets.eu>
2019-01-29 15:24:11 +0530

    Adding In memory security

--- SpringSecurity/src/main/java/com/varun/controller/HomeController.java
+++ SpringSecurity/src/main/java/com/varun/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.varun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -9,6 +10,11 @@ public class HomeController {
 	@RequestMapping("/home")
 	public String getHomePage() {
 		return "home";
+	}
+	
+	@RequestMapping(value="/private" ,method=RequestMethod.GET)
+	public String getPrivatePage() {
+		return "private";
 	}
 
 }
